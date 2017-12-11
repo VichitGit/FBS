e52e03c8cee282129bf8881a42c839908a092da0
vichitpovgit <povvichit.git@gmail.com>
2017-12-12 01:26:38 +0700

    Complete ui design login, register, verification code.

---
+++ app/src/main/java/vichitpov/com/fbs/ui/activity/StartLoginActivity.java
@@ -0,0 +1,15 @@
+package vichitpov.com.fbs.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import vichitpov.com.fbs.R;
+
+public class StartLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_login);
+    }
+}
