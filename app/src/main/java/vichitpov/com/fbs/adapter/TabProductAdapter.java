ca9d2e9d2285cc582ee75b5c59671db1722e0bdd
vichitpovgit <povvichit.git@gmail.com>
2017-12-18 00:41:04 +0700

    sell product and buy product design ui

---
+++ app/src/main/java/vichitpov/com/fbs/adapter/TabProductAdapter.java
@@ -0,0 +1,47 @@
+package vichitpov.com.fbs.adapter;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import vichitpov.com.fbs.model.TabModel;
+
+/**
+ * Created by Goldenware on 12/16/2017.
+ */
+
+public class TabProductAdapter extends FragmentPagerAdapter {
+    List<TabModel> tabList;
+    Context context;
+
+    public TabProductAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        tabList = new ArrayList<>();
+        this.context = context;
+
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return tabList.get(position).getFragment();
+    }
+
+    @Override
+    public int getCount() {
+        return tabList.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabList.get(position).getTitle();
+    }
+
+    public void addTab(TabModel tab) {
+        tabList.add(tab);
+
+    }
+}
---
+++ app/src/main/java/vichitpov/com/fbs/ui/fragments/SellProductFragment.java
@@ -0,0 +1,30 @@
+package vichitpov.com.fbs.ui.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import vichitpov.com.fbs.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SellProductFragment extends Fragment {
+
+
+    public SellProductFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sell_product, container, false);
+    }
+
+}
