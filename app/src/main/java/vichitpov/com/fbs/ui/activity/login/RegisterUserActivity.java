07f547f5d167911c62e4551fc5f835a3a3cc5774
vichitpovgit <v.vichitpov@gmail.com>
2018-02-12 00:24:15 +0700

    update ui

--- app/src/main/java/vichitpov/com/fbs/ui/activity/login/RegisterUserActivity.java
+++ app/src/main/java/vichitpov/com/fbs/ui/activity/login/RegisterUserActivity.java
@@ -1,15 +1,15 @@
-package vichitpov.com.fbs.ui.activity;
+package vichitpov.com.fbs.ui.activity.login;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
 import vichitpov.com.fbs.R;
 
-public class LoginDifferentActivity extends AppCompatActivity {
+public class RegisterUserActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_diffrent);
+        setContentView(R.layout.activity_register_user);
     }
 }
---
+++ app/src/main/java/vichitpov/com/fbs/ui/activity/login/StartLoginActivity.java
@@ -0,0 +1,19 @@
+package vichitpov.com.fbs.ui.activity.login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import vichitpov.com.fbs.R;
+
+public class StartLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_login);
+
+    }
+}
